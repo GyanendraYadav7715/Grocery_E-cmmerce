8ea5616512f643711b253d8fe5949afa8f96405c
Gyan <gyanendrayadav12345789@gmail.com>
2025-06-07 16:30:08 +0530

    inital server folder and required npm packeages setup is done

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+
+const app = express();
+const allowedOrigins = ['http://localhost:5173'] //Allow multiple origins
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+//middleware configuration
+app.use(express.json());
+app.use(cookieParser())
+app.use(cors({origin:allowedOrigins,credentials:true}))
+
+
+app.get("/", (req, res) => {
+    res.send("hello server")
+})
+app.listen(PORT,() => {
+    console.log(`server is running on port ${PORT}`)
+})
